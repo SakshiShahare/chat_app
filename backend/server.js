6cac5c7f35738f48c9b6801d23cddf5dd32769f3
sakshi shahare <sakshishahare02@gmail.com>
2024-06-24 13:46:09 +0530

    errors resolved

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import cors from "cors";
+// import cors from "cors";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: true}));
+// app.use(cors({credentials: true, origin: true}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -9,7 +9,7 @@ const useGetConversations = () => {
 		const getConversations = async () => {
 			setLoading(true);
 			try {
-				const res = await fetch("http://localhost:8080/api/users" , 
+				const res = await fetch("/api/users" , 
 					{withCredntials: true,
 					credentials: 'include'});
 				const data = await res.json();
